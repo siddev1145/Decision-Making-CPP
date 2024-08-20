83c650b914f76df50839ab49afede1f68d0a9ece
Siddhant Bedre <siddhantbedre2004@gmail.com>
2024-08-20 23:44:21 +0530

    Create odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,21 @@
+//Name: Siddhant Bedre
+//PRN: 23070123037
+//AIM - To check whether a number is even or odd.
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+  int a;
+  cout << "Enter a number: ";
+  cin >> a;
+if (a%2 == 0)
+{ cout << "The number is even.";}
+else
+{cout << "The number is not even.";}
+    return 0;
+}
+
+/* output-
+Enter a number: 4
+The number is even.*/
